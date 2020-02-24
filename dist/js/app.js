df3eb2096f756df0b06e02980f850e1cd70a170d
monmonplaza <monmon.plaza@gmail.com>
2020-02-24 13:10:08 +0800

    deployment

--- dist/js/app.js
+++ dist/js/app.js
@@ -42,10 +42,58 @@ function counters(limit, target) {
   }, 10 / 1000);
 }
 
+function skillbar(css, html, js, php, ps, ai, xd) {
+  this.css = css;
+  this.html = html;
+  this.js = js;
+  this.php = php;
+  this.ps = ps;
+  this.ai = ai;
+  this.xd = xd;
+  this.createSkillBar = function() {
+    let skillArray = [
+      "HTML",
+      "CSS",
+      "Javascript",
+      "PHP",
+      "Photoshop",
+      "Illustrator",
+      "XD"
+    ];
+
+    let percent = [
+      this.html,
+      this.css,
+      this.js,
+      this.php,
+      this.ps,
+      this.ai,
+      this.xd
+    ];
+
+    let html = "";
+
+    for (let i = 0; i <= skillArray; i++) {
+      html = `
+      <div class="skill-group">
+        <p>${skillArray[i]}</p>
+        <div class="bar">
+          <div class="bar-percent" style="width=${percent[i]}"></div>
+        </div>
+      </div>`;
+    }
+
+    document.querySelector(".skill-wrapper").innerHTML += html;
+  };
+}
+
+let monmon = new skillbar();
+document.querySelector("#tab-4").addEventListener("click", function() {
+  monmon.createSkillBar(54, 76, 34, 86, 23, 54);
+});
+
 //SCROLL MAGIC
-
 var controller = new ScrollMagic.Controller();
-
 // create a scene
 new ScrollMagic.Scene({
   // duration: 100, // the scene should last for a scroll distance of 100px
